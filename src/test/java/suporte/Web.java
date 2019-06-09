2cca74c118cf4a78f7f62e2759c850e587588732
Roberto Atanásio <roberto.atanasio.pl@gmail.com>
2019-06-08 21:54:01 -0300

    commit final

--- src/test/java/suporte/Web.java
+++ src/test/java/suporte/Web.java
@@ -2,10 +2,18 @@ package suporte;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Web {
+
+    public static final String USERNAME = null;
+    public static final String AUTOMATE_KEY = null;
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 
     public static WebDriver createChrome() {
         System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDriver\\chromedriver.exe");
@@ -13,6 +21,27 @@ public class Web {
         navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);  // espera até 5 segundos para carregar os itens da página
         navegador.manage().window().maximize();
         navegador.get("http://www.juliodelima.com.br/taskit");  // abrir o site
+        return navegador;
+    }
+
+    public static WebDriver createBrowserStack() {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("browser", "Chrome");
+        caps.setCapability("browser_version", "75.0 beta");
+        caps.setCapability("os", "Windows");
+        caps.setCapability("os_version", "10");
+        caps.setCapability("resolution", "1024x768");
+
+        WebDriver navegador = null;
+        try {
+            navegador = new RemoteWebDriver(new URL(URL), caps);
+            navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            navegador.get("http://www.juliodelima.com.br/taskit");
+        } catch (MalformedURLException e) {
+            System.out.println("Houve erro na criação do WebDriver: " + e.getMessage());
+        }
+
         return navegador;
     }
 }
